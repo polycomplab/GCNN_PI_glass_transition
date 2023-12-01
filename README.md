baf522b7fefc29df96ac4556a43d5e2de6edb2c7
pavelbatyr <pavelbatyr@gmail.com>
2023-12-01 14:29:22 +0000

    update Tg predictions script

--- README.md
+++ README.md
@@ -22,3 +22,23 @@ python3 main.py configs/config_pretrain_on_subset_perm.py
 ```
 python3 main.py configs/config_finetune_perm.py
 ```
+
+### Prediction:
+```
+python3 scripts/predict.py --checkpoint_path "checkpoints/finetune/trained_network_weights.pth"
+```
+Create a file datasets/prediction/SMILES.csv and save a list of SMILES to it, one SMILES string per line.
+
+Examples of SMILES.csv:
+```
+Ic1ccc(cc1)Oc1ccc(cc1)N1C(=O)c2c(C1=O)cc(cc2)Oc1cccc2c1cccc2Oc1ccc2c(c1)C(=O)N(C2=O)I
+Ic1cccc(c1)N1C(=O)c2c(C1=O)cc(cc2)Oc1cccc2c1cccc2Oc1ccc2c(c1)C(=O)N(C2=O)I
+```
+Predicted Tg values will be saved to datasets/prediction/predictions.csv
+
+Example of predictions.csv:
+```
+SMILES,"Tg, K, pred"
+Ic1ccc(cc1)Oc1ccc(cc1)N1C(=O)c2c(C1=O)cc(cc2)Oc1cccc2c1cccc2Oc1ccc2c(c1)C(=O)N(C2=O)I,524.7864379882812
+Ic1cccc(c1)N1C(=O)c2c(C1=O)cc(cc2)Oc1cccc2c1cccc2Oc1ccc2c(c1)C(=O)N(C2=O)I,535.5695190429688
+```
--- scripts/predict.py
+++ scripts/predict.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import shutil
 
@@ -9,56 +10,65 @@ from data.prediction_dataset import PredictionExperimentalDataset
 from models import KGNNModel
 
 
-checkpoint_path = ('checkpoints/finetune/100_epochs_lr_0.0006_round_20_new_db_'
-                   'finetune without pretrain #Tue Sep 26 00:08:35 2023_split_0_last.pth')
+def predict(checkpoint_path):
 
-model = KGNNModel()
-checkpoint = torch.load(checkpoint_path)
+    model = KGNNModel()
+    checkpoint = torch.load(checkpoint_path)
 
-# mean and standard deviation of target property values
-mean = checkpoint['mean']
-std = checkpoint['std']
+    # mean and standard deviation of target property values
+    mean = checkpoint['mean']
+    std = checkpoint['std']
 
-model.load_state_dict(checkpoint['model_state_dict'])
+    model.load_state_dict(checkpoint['model_state_dict'])
 
 
-predictions_path = 'datasets/prediction/'
-smiles_path = predictions_path + 'SMILES.csv'
-temp_dataset_path = predictions_path + 'temp/'
-dataset_path_raw = temp_dataset_path+'raw/'
-dataset_path_processed = temp_dataset_path+'processed/'
-os.makedirs(dataset_path_raw, exist_ok=True)
-os.makedirs(dataset_path_processed, exist_ok=True)
-shutil.copy(smiles_path, dataset_path_raw)
+    predictions_path = 'datasets/prediction/'
+    smiles_path = predictions_path + 'SMILES.csv'
+    temp_dataset_path = predictions_path + 'temp/'
+    dataset_path_raw = temp_dataset_path+'raw/'
+    dataset_path_processed = temp_dataset_path+'processed/'
+    os.makedirs(dataset_path_raw, exist_ok=True)
+    os.makedirs(dataset_path_processed, exist_ok=True)
+    shutil.copy(smiles_path, dataset_path_raw)
 
-target_name = 'Tg, K'
-dataset = PredictionExperimentalDataset(
-    root=temp_dataset_path,
-    target_name=target_name,
-    mean=mean,
-    std=std)
-test_loader = DataLoader(
-            dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=1)
+    target_name = 'Tg, K'
+    dataset = PredictionExperimentalDataset(
+        root=temp_dataset_path,
+        target_name=target_name,
+        mean=mean,
+        std=std)
+    test_loader = DataLoader(
+                dataset,
+                batch_size=1,
+                shuffle=False,
+                num_workers=1)
 
-model.eval()
-data_format = {"SMILES": [], f"{target_name}, pred": []}
-preds_result_df = pd.DataFrame(data_format)
-with torch.no_grad():
-    for i, test_data in enumerate(test_loader):
-        pred = model(test_data).squeeze()
-        smiles = dataset.ids_smiles[test_data.index.item()]
-        pred_denormalized = (pred*std + mean).cpu().numpy().tolist()
-        data_batch = {
-            "SMILES": [smiles],
-            f"{target_name}, pred": [pred_denormalized]}
-        print(data_batch)
-        preds_result_df = preds_result_df.append(
-            pd.DataFrame(data_batch), ignore_index=True)
-                
-results_save_path = f'{predictions_path}/predictions.csv'
-preds_result_df.to_csv(results_save_path, index=False)
+    model.eval()
+    data_format = {"SMILES": [], f"{target_name}, pred": []}
+    preds_result_df = pd.DataFrame(data_format)
+    with torch.no_grad():
+        for i, test_data in enumerate(test_loader):
+            pred = model(test_data).squeeze()
+            smiles = dataset.ids_smiles[test_data.index.item()]
+            pred_denormalized = (pred*std + mean).cpu().numpy().tolist()
+            data_batch = {
+                "SMILES": [smiles],
+                f"{target_name}, pred": [pred_denormalized]}
+            print(data_batch)
+            preds_result_df = preds_result_df.append(
+                pd.DataFrame(data_batch), ignore_index=True)
+                    
+    results_save_path = f'{predictions_path}/predictions.csv'
+    preds_result_df.to_csv(results_save_path, index=False)
 
-shutil.rmtree(temp_dataset_path)
+    shutil.rmtree(temp_dataset_path)
+
+
+if __name__ == '__main__':
+    # checkpoint_path = ('checkpoints/finetune/100_epochs_lr_0.0006_round_20_new_db_'
+    #                    'finetune without pretrain #Tue Sep 26 00:08:35 2023_split_0_last.pth')
+                    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint_path", type=str)
+    args = parser.parse_args()
+    predict(args.checkpoint_path)
